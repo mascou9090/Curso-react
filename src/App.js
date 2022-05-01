15647fa14edeefaa9e0b7013d37a6a01d6966e6c
Marcondes Ferreira Pinheiro <mascou@pop-os.localdomain>
2022-05-01 14:02:10 -0300

    secondCommit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
 import React from 'react';
-import Select from './Form/Select';
-import Input from './Form/Input';
-import Radios from './Form/Radios';
-import Checkbox from './Form/Checkbox';
+import './App.css'
+import { Produto } from './Produto';
 
 const App = () => {
-  const [nome, setNome] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('');
-  const [cor, setCor] = React.useState('');
-  const [frutas, setFrutas] = React.useState('');
-  const [linguagens, setLinguagens] = React.useState([]);
-  const [termos, setTermos] = React.useState([]);
-
+  const [ativar, setAtivar] = React.useState(false);
 
   return (
-    <form>
-      <h2>Termos</h2>
-      <Checkbox options={['Li e aceitos os termos']} value={termos} setValue={setTermos}/>
-
-      <h2>CheckBox</h2>
-      <Checkbox options={['JavaScript', 'PHP', 'Ruby']} value={linguagens} setValue={setLinguagens}/>
-      <h2>Cores</h2>
-      <Radios options={['Azul', 'Vermelho']} value={cor} setValue={setCor}/>
-      <h2>Frutas</h2>
-      <Radios options={['Laranja', 'Limão', 'Uva']} value={frutas} setValue={setFrutas}/>
-      <h2>Itens</h2>
-      <Select options={['Smartphone', 'Tablet', 'Notebook']} value={produto} setValue={setProduto}/>
-      <Input id="nome" label="Nome" value={nome} setValue={setNome}/>
-      <Input id="email" label="Email" value={email} setValue={setEmail}/>
-      <button>Enviar</button>
-    </form>
+    <div>
+      <button onClick={() => setAtivar(!ativar)}>Ativar</button>
+      {ativar && <Produto/>}
+    </div>
   );
 };
 
 export default App;
+ 
---
+++ src/Produto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Produto = () => {
+  return (
+      <div className="animeLeft">
+          <h1>Produto</h1>
+          <p>Meu produto</p>
+      </div>
+  )
+}
